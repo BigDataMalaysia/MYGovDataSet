b6df900869c845880fed638c206db148fad2083d
Tirath Ramdas <tirath@int19h.com>
2014-07-16 13:05:59 +1000

    HACK/WIP: parse a sample raw file.
    With this we can experiment with asset class extraction. With this
    sample data, we expect to detect a PDF.

---
+++ hackxperiment_asset_type.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import mygovdataset
+
+with open("dataset53.txt", "r") as dataset_raw:
+    dataset = mygovdataset.MYGovDataSet(url=None, payload_text=dataset_raw.read())
+
+print dataset
+
+print "---------- updated: {0} views: {1} ----------".format(dataset.last_updated, dataset.view_count)
+print dataset.org_metadata
